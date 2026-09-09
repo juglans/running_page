d88627e146562b2388e57ddfcec760391f154ff1
juglans <juglans@users.noreply.github.com>
2026-09-09 20:26:01 +0800

    feat(map): turn lights on by default so basemap shows on load

--- src/themes/classic/utils/const.ts
+++ src/themes/classic/utils/const.ts
@@ -39,9 +39,8 @@ const ROAD_LABEL_DISPLAY = true;
 // updated on 2024/11/17: privacy mode is set to true by default
 //set to `true` if you want to display only the routes without showing the map.
 const PRIVACY_MODE = false;
-// updated on 2024/11/17: lights are turned off by default
-//set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
-const LIGHTS_ON = false;
+// lights default state, only effect when `PRIVACY_MODE` = false
+const LIGHTS_ON = true;
 //set to `true` if you want to show the 'Elevation Gain' column
 const SHOW_ELEVATION_GAIN = false;
 // richer title for the activity types (like garmin style)
